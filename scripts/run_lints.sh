46936261f863ac7aa21368edf468641353e35983
Evgeniy Firsov <firsov@uber.com>
2017-06-29 22:51:07 -0700

    Run lints sequentially and report result

--- scripts/run_lints.sh
+++ scripts/run_lints.sh
@@ -2,4 +2,6 @@
 
 TIMEOUT=300s
 
-CGO_ENABLED=0 gometalinter -e format_gen --deadline=$TIMEOUT --disable-all -Evet -Egolint -Egoimports -Eineffassign -Egosimple -Eerrcheck -Eunused -Edeadcode -Emisspell -Egocyclo --cyclo-over=15 $@
+for i in $@; do
+	CGO_ENABLED=0 gometalinter -e format_gen --deadline=$TIMEOUT --disable-all -Evet -Egolint -Egoimports -Eineffassign -Egosimple -Eerrcheck -Eunused -Edeadcode -Emisspell -Egocyclo --cyclo-over=15 $i && echo "ok\t$i"
+done

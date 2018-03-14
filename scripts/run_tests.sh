b4eb24963e8d5006c907ee190eee69e4649ca1d0
Evgeniy Firsov <firsov@uber.com>
2018-03-13 17:50:08 -0700

    Disable coverage by setting NOCOVER env variable for run_tests.sh

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -7,8 +7,12 @@ TIMEOUT=300s
 
 export GOTRACEBACK="crash" #produce core file on panic
 
+if [ -z "$NOCOVER" ]; then
+	COVER="-coverprofile=profile.out -covermode=atomic"
+fi
+
 #FIXME: Because of the shared state in database tests can't be run in parallel
-CMD="go test -race -coverprofile=profile.out -covermode=atomic -test.timeout $TIMEOUT"
+CMD="go test -race $COVER -test.timeout $TIMEOUT"
 
 for i in $@; do
 	$CMD $TEST_PARAM $i || exit 1
